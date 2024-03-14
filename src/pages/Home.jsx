b98f18ce8542a96d52df669a73b2e36515b7513f
amanmohammadmk <93486056+amanmohammadmk@users.noreply.github.com>
2024-03-14 06:26:37 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -72,7 +72,8 @@ function Home() {
                 <ul>
                     <li>Aa endhina ummane aloikumbam tension. Athonn mattane. Pinne kaka sammaykatha kakane parajit karyalla endhelum oru quality ink vende . 
 Nte emaanum ippam koyappalla . Kakane sammaypikkan endha cheyyande oru idea kittalla.
-Endhayalum nallonam durakke padachone nalloru vazhi kanicharum. Pinne endh cheyyumbalum emaanne bathikkathe nokkane athan main .</li>
+Endhayalum nallonam durakke padachone nalloru vazhi kanicharum. Pinne endh cheyyumbalum emaanne bathikkathe nokkane athan main . ith ink evide delete akkan kayyum . onn nalla reethil ellarum sammaycha mathiyeni. nji nallonam duarke athe ullu. 
+                     endh ndelum parayanam tto. aa thatha pinne preshnam onnum ndakki vennillalo. </li>
                     <li>Assalamualaikkum,
 Emaan okke settalle ath nalle pole maintain cheyye baki okke padachon tharum.
 
